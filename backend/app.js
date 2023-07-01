c7340deea8ed97e8e5e500ee1d40b8720f68a286
Gorlov Mark <masmik@rambler.ru>
2023-07-01 15:05:27 +0300

    add 15.1.6

--- backend/app.js
+++ backend/app.js
@@ -2,24 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
 const { errors } = require("celebrate");
 const cors = require("cors");
 const { requestLogger, errorLogger } = require("./middlwares/logger");
 const router = require("./routes");
 const GlobalError = require("./middlwares/GlobalError");
+const appLimiter = require("./settings/settingsLimiter");
 
 const { PORT = 3000, WEB_HOST = "mongodb://127.0.0.1:27017/mestodb" } =
   process.env;
 const app = express();
-
-
-const appLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
 
 mongoose.connect(WEB_HOST, {
   useNewUrlParser: true,
@@ -30,10 +22,10 @@ app.get('/crash-test', () => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
+app.use(requestLogger);
 app.use(appLimiter);
 app.use(helmet());
 app.use(express.json());
-app.use(requestLogger);
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
