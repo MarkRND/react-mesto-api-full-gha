7dfb55346a173126303810eb0eac07a0cef439e9
Gorlov Mark <masmik@rambler.ru>
2023-07-01 15:26:10 +0300

    add 15.1.8

--- backend/middlwares/logger.js
+++ backend/middlwares/logger.js
@@ -1,16 +1,23 @@
-const winston = require('winston');
-const expressWinston = require('express-winston');
+const path = require("path");
+const winston = require("winston");
+const expressWinston = require("express-winston");
+
+const logsDirectory = path.join(__dirname, "logs");
 
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: '../logs/request.log' }),
+    new winston.transports.File({
+      filename: path.join(logsDirectory, "request.log"),
+    }),
   ],
   format: winston.format.json(),
 });
-// логгер ошибок
+
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: '../logs/error.log' }),
+    new winston.transports.File({
+      filename: path.join(logsDirectory, "error.log"),
+    }),
   ],
   format: winston.format.json(),
 });
