b0c1568aab5bedbebb1bfebccf47404b1c7766ea
Gorlov Mark <masmik@rambler.ru>
2023-07-01 03:25:55 +0300

    add 15.0.16

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,11 +9,7 @@ const {
   validationSignup,
 } = require("../middlwares/celebrateJoi");
 
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+
 
 router.post("/signup", validationSignup, addUser);
 router.post("/signin", validationSignin, login);
